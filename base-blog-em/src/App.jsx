132448175a3d60053d7fdd875ea9a073f606f15f
eojine94 <eojine294@gmail.com>
2023-03-02 23:32:21 +0900

    add : put basic of react-query like client and provider

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,14 +1,22 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { Posts } from './Posts';
 import './App.css';
 
-// 0302 - 1
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     // provide React Query client to App
-    <div className='App'>
-      <h1>Blog Posts</h1>
-      <Posts />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className='App'>
+        <h1>Blog Posts</h1>
+        <Posts />
+      </div>
+
+      {/* React Query Devtools */}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,23 +1,15 @@
 async function fetchComments(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
-  );
+  const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`);
   return response.json();
 }
 
 async function deletePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "DELETE" }
-  );
+  const response = await fetch(`https://jsonplaceholder.typicode.com/postId/${postId}`, { method: 'DELETE' });
   return response.json();
 }
 
 async function updatePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
-  );
+  const response = await fetch(`https://jsonplaceholder.typicode.com/postId/${postId}`, { method: 'PATCH', data: { title: 'REACT QUERY FOREVER!!!!' } });
   return response.json();
 }
 
@@ -27,7 +19,7 @@ export function PostDetail({ post }) {
 
   return (
     <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <h3 style={{ color: 'blue' }}>{post.title}</h3>
       <button>Delete</button> <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
